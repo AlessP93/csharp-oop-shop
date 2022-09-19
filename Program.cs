cac407f29f98f0163a36e639e698dbf2a14dcf9e
AlessP93 <alespiscino@libero.it>
2022-09-19 16:57:24 +0200

    fine esercizio

--- Program.cs
+++ Program.cs
@@ -1,25 +1,101 @@
 ﻿
 Prodotto UtenteProdotto = new Prodotto();
 
-UtenteProdotto.codice = new Random().Next(0, 100000000);
-UtenteProdotto.nome = Console.ReadLine();
-UtenteProdotto.descrizione = Console.ReadLine();
-UtenteProdotto.prezzo = Convert.ToInt32(Console.ReadLine());
-UtenteProdotto.iva = Convert.ToInt32(Console.ReadLine());
+UtenteProdotto.SetCodice(new Random().Next(0, 100));
+UtenteProdotto.SetNome("Iphone");
+UtenteProdotto.SetDescrizione("Iphone 13 pro max 512gb black");
+UtenteProdotto.SetPrezzo(1399);
+UtenteProdotto.SetIva(22);
 
-Console.WriteLine(UtenteProdotto.codice);
-Console.WriteLine("inserisci un nome ", UtenteProdotto.nome);
-Console.WriteLine("inserisci un descrizione ", UtenteProdotto.descrizione);
-Console.WriteLine("inserisci l' iva di 11 numeri ", UtenteProdotto.iva);
+Console.WriteLine("Codice del prodotto");
+Console.WriteLine(UtenteProdotto.GetCodice());
+Console.WriteLine();
+Console.WriteLine("Nome del prodotto");
+Console.WriteLine(UtenteProdotto.GetNome());
+Console.WriteLine();
+Console.WriteLine("Descrizione del prodotto");
+Console.WriteLine(UtenteProdotto.GetDescrizione());
+Console.WriteLine();
+Console.WriteLine("Prezzo del prodotto");
+Console.WriteLine(UtenteProdotto.GetPrezzo() + "$");
+Console.WriteLine();
+Console.WriteLine("Iva del prodotto");
+Console.WriteLine(UtenteProdotto.GetIva() + "%");
+Console.WriteLine();
+Console.WriteLine("Prodotto con iva");
+Console.WriteLine(UtenteProdotto.functionIva());
+Console.WriteLine();
+Console.WriteLine("Codice + nome");
+Console.WriteLine(UtenteProdotto.concatenazione());
+
 
 
 //crea un prodotto caratterizzato da:
 public class Prodotto
 {
-    public int codice;
-    public string nome;
-    public string descrizione;
-    public double prezzo;
-    public int iva;
+    private int codice;
+    private string nome;
+    private string descrizione;
+    private double prezzo;
+    private int iva;
+
+    // setter
+    public void SetCodice(int codice)
+    {
+        this.codice = codice;
+    }
+
+    public void SetNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public void SetDescrizione(string descrizione)
+    {
+        this.descrizione = descrizione;
+    }
+
+    public void SetPrezzo(double prezzo)
+    {
+        this.prezzo = prezzo;
+    }
+
+    public void SetIva(int iva)
+    {
+        this.iva = iva;
+    }
+
+    //getter
+    public int GetCodice()
+    {
+        return codice;
+    }
+    public string GetNome()
+    {
+        return nome;
+    }
+    public string GetDescrizione()
+    {
+        return descrizione;
+    }
+
+    public double GetPrezzo()
+    {
+        return prezzo;
+    }
+
+    public int GetIva()
+    {
+        return iva;
+    }
+    //methods
+    public double functionIva()
+    {
+        return GetPrezzo() + (GetIva() / 100);
+    }
+    public string concatenazione()
+    {
+        return GetCodice() + " - " + GetNome();
+    }
 }
 
